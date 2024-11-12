e667135f8f911ee72b61b89f3e58fa0270e7578c
Stéphane Nsakala <stephane.nsakala@hopitauxschuman.lu>
2024-11-12 12:57:49 +0100

    handle -s option

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
-
+use clap::{Parser, Subcommand};
 use wake_on_lan::wol::wake_on_lan;
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    device: Option<String>,
+
+    #[arg(short, long)]
+    source_ip: Option<String>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    CreateAlias { mac: String, alias: String },
+    RemoveAlias { alias: String },
+    SetDefaultAlias { alias: String },
+    SetDefaultSourceIp { source_ip: String },
+}
+
 fn main() {
+    let cli = Cli::parse();
     let default_mac = "2c:f0:5d:e1:9e:d6";
-    wake_on_lan(&default_mac, Some("172.18.100.68"));
-    
+    if let None = cli.command {
+        wake_on_lan(default_mac, cli.source_ip.as_deref());
+    }
 }
